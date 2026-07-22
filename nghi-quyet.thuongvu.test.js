e50805215366913bd38f961ec18f1ce38713bde9
Viet Bach Pham <phamvietbach2006@gmail.com>
2026-07-22 22:13:19 +0700

    Cập nhật dự án

--- nghi-quyet.thuongvu.test.js
+++ nghi-quyet.thuongvu.test.js
@@ -7,15 +7,21 @@ const helperBlock = html.slice(
   html.indexOf('        function parseDate(raw) {'),
   html.indexOf('        async function previewData() {')
 );
+const docxLayoutBlock = html.slice(
+  html.indexOf('        function fixIctKinhGuiLayout(docXml) {'),
+  html.indexOf('        function showCustomModal(options) {')
+);
 
-assert(helperBlock, 'Could not find helpers in nghi-quyet.html');
+assert(helperBlock && docxLayoutBlock, 'Could not find helpers in nghi-quyet.html');
 
 const sandbox = {};
-vm.runInNewContext(`${helperBlock}
+vm.runInNewContext(`${docxLayoutBlock}
+${helperBlock}
 this.parseDate = parseDate;
 this.getColValue = getColValue;
 this.getMissingColumns = getMissingColumns;
-this.isValidDateValue = isValidDateValue;`, sandbox);
+this.isValidDateValue = isValidDateValue;
+this.fixIctKinhGuiLayout = fixIctKinhGuiLayout;`, sandbox);
 
 const headers = [
   'Họ tên',
@@ -43,5 +49,15 @@ assert.strictEqual(sandbox.parseDate(ngayHopRaw), 'ngày 06 tháng 07 năm 2026'
 assert.strictEqual(sandbox.parseDate(thuongVuRaw), 'ngày 08 tháng 07 năm 2026');
 assert.strictEqual(sandbox.isValidDateValue(thuongVuRaw), true);
 assert(html.includes('ThoiGianHopThuongvu: student.thoiGianHopThuongVu'));
+
+const sampleNghiQuyetXml = [
+  '<w:p><w:pPr><w:ind w:firstLine="720"/></w:pPr><w:r><w:t xml:space="preserve"> </w:t></w:r><w:r><w:t xml:space="preserve">              </w:t></w:r><w:r><w:t xml:space="preserve">Kính </w:t></w:r><w:r><w:t xml:space="preserve">gửi:    </w:t></w:r><w:r><w:t xml:space="preserve">- </w:t></w:r><w:r><w:t>C</w:t></w:r><w:r><w:t>hi</w:t></w:r><w:r><w:t xml:space="preserve"> ủy Chi</w:t></w:r><w:r><w:t xml:space="preserve"> bộ </w:t></w:r><w:r><w:t>Sinh viên</w:t></w:r><w:r><w:t xml:space="preserve"> </w:t></w:r><w:r><w:t>Công nghệ Thông tin và Truyền thông</w:t></w:r><w:r><w:t>;</w:t></w:r></w:p>',
+  '<w:p><w:r><w:t xml:space="preserve">                            </w:t></w:r><w:r><w:t xml:space="preserve">    </w:t></w:r><w:r><w:t xml:space="preserve">              </w:t></w:r><w:r><w:t xml:space="preserve">- Đảng </w:t></w:r><w:r><w:t>uỷ</w:t></w:r><w:r><w:t xml:space="preserve"> Đại học Bách khoa Hà Nội.</w:t></w:r></w:p>'
+].join('');
+const fixedNghiQuyetXml = sandbox.fixIctKinhGuiLayout(sampleNghiQuyetXml);
+assert(!fixedNghiQuyetXml.includes('w:firstLine="720"'));
+assert(fixedNghiQuyetXml.includes(`<w:t xml:space="preserve">${' '.repeat(13)}Kính </w:t>`));
+assert(fixedNghiQuyetXml.includes('<w:t xml:space="preserve">gửi:</w:t></w:r><w:r><w:t xml:space="preserve"> - </w:t>'));
+assert(fixedNghiQuyetXml.includes(`<w:t xml:space="preserve">${' '.repeat(12)}- Đảng </w:t>`));
 
 console.log('Nghị quyết Thường vụ check passed');
